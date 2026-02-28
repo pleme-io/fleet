e8849ebd09800616e2a18afbd7264e5fa137c95c
drzzln <drzzln@protonmail.com>
2026-02-27 21:12:30 -0500

    chore: switch nixpkgs to nixos-25.11 stable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Fleet — NixOS fleet lifecycle CLI with DAG workflow orchestration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
